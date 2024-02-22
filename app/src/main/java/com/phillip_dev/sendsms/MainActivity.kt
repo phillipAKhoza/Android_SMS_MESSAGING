68a6408d5c5363d087caa5a4054ffa5db5f50d4f
phillip khoza <phillipantonk@gmail.com>
2024-02-22 14:37:39 +0200

    declare ui variables

--- app/src/main/java/com/phillip_dev/sendsms/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/sendsms/MainActivity.kt
@@ -2,8 +2,13 @@ package com.phillip_dev.sendsms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var message : EditText
+    lateinit var phone: EditText
+    lateinit var send: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
