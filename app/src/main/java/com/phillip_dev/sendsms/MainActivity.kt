29fe22d43bb420d6faa5b17a75b32ca05b9c2fcf
phillip khoza <phillipantonk@gmail.com>
2024-02-22 15:15:04 +0200

    send message

--- app/src/main/java/com/phillip_dev/sendsms/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/sendsms/MainActivity.kt
@@ -2,12 +2,15 @@ package com.phillip_dev.sendsms
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
     lateinit var message : EditText
@@ -35,7 +38,15 @@ class MainActivity : AppCompatActivity() {
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS),1)
         }else{
+            val smsManager : SmsManager
 
+            if(Build.VERSION.SDK_INT >= 23){
+                smsManager = this.getSystemService(SmsManager::class.java)
+            }else{
+                smsManager = SmsManager.getDefault()
+            }
+
+            smsManager.sendTextMessage(userPhone,null,userMessage,null,null,)
         }
 
     }
